15ea8e0aa7901825965e03844194120be4704549
Ash Nayak <your-email@example.com>
2026-06-12 15:31:24 +0530

    feat: configure server to listen on all network interfaces in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone'
   }),
+  server: {
+    host: true
+  },
   integrations: [sitemap(), skills()],
   vite: {
     plugins: [tailwindcss()],
